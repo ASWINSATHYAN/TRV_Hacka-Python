54d4be716e12c3bff7538b9aaf57ecb30da1b750
HARLANDFS\Aswin.Sathyavageeswa <aswin.sathyavageeswaran@dh.com>
2018-12-05 18:49:25 +0530

    Commented out unwanted code.

--- application.py
+++ application.py
@@ -47,24 +47,22 @@ def predictCourse():
                 applicationData[data] = 1
             else:
                 applicationData[data] = 0
-    with open("data.csv", 'r') as resultFile:
-        lines = list(resultFile)
-    resultFile.close();
-    if (len(lines)) > 0:
-        values = lines[2].split(",")
-        for index, data in enumerate(list(applicationData.values())):
-            values[index] = data
-        applicationData = values
-
+    # with open("data.csv", 'r') as resultFile:
+    #     lines = list(resultFile)
+    # resultFile.close();
+    # if (len(lines)) > 0:
+    #     values = lines[2].split(",")
+    #     for index, data in enumerate(list(applicationData.values())):
+    #         values[index] = data
+    #     applicationData = values
     with open("data.csv", 'w') as resultFile:
-        if resultFile is not None:
-            wr = csv.writer(resultFile, dialect='excel')
-            if isinstance(applicationData, dict):
-                wr.writerow(applicationData.keys())
-                wr.writerow(applicationData.values())
-            else:
-                wr.writerow(json.loads(request.data).keys())
-                wr.writerow(applicationData)
+        wr = csv.writer(resultFile, dialect='excel')
+        # if isinstance(applicationData, dict):
+        wr.writerow(applicationData.keys())
+        wr.writerow(applicationData.values())
+        # else:
+        #     wr.writerow(json.loads(request.data).keys())
+        #     wr.writerow(applicationData)
     resultFile.close();
     predictedCourses = getCourse()
     response = {"Courses": predictedCourses}
